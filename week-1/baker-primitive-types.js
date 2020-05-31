dd3beda824a97923a2cb79f492c1199e54ba1fbe
Tiffany Baker <tkbaker@my365.bellevue.edu>
2020-05-30 20:39:02 -0500

    adding baker-primitive-types.js

---
+++ week-1/baker-primitive-types.js
@@ -0,0 +1,26 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Tiffany Baker
+; Date: 30-may-2020
+; Modified By: tiffany Baker
+; Description: This program demonstrates the
+; use of JavaScript values and
+; and variables.
+;===========================================
+*/ 
+
+
+var firstName = "Tiffany";
+var lastName = "Baker";
+var age = "34";
+var street = "1327 limber rd";
+var city = "June";
+var zipCode = "12684";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
