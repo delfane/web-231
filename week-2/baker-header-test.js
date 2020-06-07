bd4e9ef8345c1f175f1a12b0502d8e511063c68c
Tiffany Baker <tkbaker@my365.bellevue.edu>
2020-06-07 13:43:06 -0500

    added baker-header.js and baker-header-test.js

---
+++ week-2/baker-header-test.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso
+; Date:   6/7/2020
+; Modified By: Tiffany Baker
+; Description: tests the output of a header file
+;===========================================
+*/
+
+const header = require('../baker-header.js');
+
+console.log(header.display("Tiffany", "Baker", "exercise 2.2"));
