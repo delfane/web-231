fcea39df8180a32a272aff2e7c6a53cf68c28661
Tiffany Baker <tkbaker@my365.bellevue.edu>
2020-06-21 00:20:52 -0500

    adding week 4 and updating week 3

--- week-3/exercise 3.2.js
+++ week-3/exercise 3.2.js
@@ -1,16 +1,58 @@
-var a = "turn";
-var b = "box";
 
-//match function does not work.
-//comment about function
+var c = "1";
+var d = "2";
+var e = "3";
+var f = "4";
+var g = "5";
+var h = "6";
+
+
+var word = [d,h,e,g,c,f];
+
+//cycles through each variable.
+
+if (word.length<=6){
+  match(d,d);
+  match(e,h);
+  match(g,c);
+  match(f,f);
+  match(f,h);
+
+}
+
+
+
+//------------------------------------------------
+//function to judge if string match
+
 function match (a,b) {
 
-   if (a===b){
-     console.log("true");
-   }else{
-      console.log("false");
-    }
+if (b === a) {
+  console.log("true");
+  return logMatch(a,b);
+} else {
+  console.log("false");
+  return logMismatch(a,b);
+}
+}
 
-   }
-console.log("hi....fix match function");
+
+
+
+// gives a message if the variables do not match.
+function logMismatch (a,b) {
+  var misMatch = "Sorry but " + a + " and " + b + " do not match.";
+  console.log(misMatch);
+}
+
+
+// gives a message if the variables do match.
+function logMatch (a,b) {
+  var doMatch = "Congrats " + a + " and " + b + " do match.";
+  console.log(doMatch);
+}
+
+
+
+
 
---
+++ week-4/exercise 4.2.js
@@ -0,0 +1,20 @@
+/*
+============================================
+; Title: exercise 4.2
+; Author: Tiffany Baker
+; Date: 6/19/2020
+; Modified By:
+; Description: This program demonstrates the
+; use of arrays.
+;===========================================
+*/
+
+//creates an array.
+const fruit= ["apple","orange","grape","peach","dragon fruit"];
+function getFruit (a) {
+  //cycles through array.
+  fruit.forEach( a => console.log(a));
+
+}
+
+console.log(getFruit(fruit));
