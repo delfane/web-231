0f3a61a5f9d8445b4db8600ba1683e4d7fe22083
Tiffany Baker <tkbaker@my365.bellevue.edu>
2020-06-25 23:27:58 -0500

    added week 5 and exercise5.2.js

---
+++ week-5/exercise5.2.js
@@ -0,0 +1,16 @@
+/*
+============================================
+; Title: discussion 5.1
+; Author: Tiffany Baker
+; Date: 6/25/2020
+; Modified By:
+; Description: This program demonstrates the
+; use of arrays with the forEach function.
+;===========================================
+*/
+
+
+let food = ["pizza","tacos","pasta","chicken","mac n cheese"];
+food.forEach(function(items){console.log(items)});
+
+
