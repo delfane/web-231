a0a5849986ced3c4064acaee63bc8e4ae0181d05
Tiffany Baker <tkbaker@my365.bellevue.edu>
2020-07-04 23:55:22 -0500

    added exercise 6.3.js

---
+++ week-6/exercise6.3.js
@@ -0,0 +1,24 @@
+/*
+============================================
+; Title: exercise 6.3
+; Author: Tiffany Baker
+; Date:7/4/2020
+; Modified By:
+; Description: this program demonstrates exception handling.
+;
+;===========================================
+*/
+
+
+const header = require('../baker-header.js');
+
+console.log(header.display("Tiffany", "Baker", "Exercise 6.2"));
+
+
+const ticket = {
+  id: "48453",
+  name: "George Anthony",
+  requestor: "Mark W."
+
+}
+console.log(ticket);
