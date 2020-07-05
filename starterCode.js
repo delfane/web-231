c277131f8e0fe6b80942c06b9aa2fc3f7b6895bd
Tiffany Baker <tkbaker@my365.bellevue.edu>
2020-07-04 20:47:52 -0500

    added week 6 and start code

---
+++ starterCode.js
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title:
+; Author: Tiffany Baker
+; Date:
+; Modified By:
+; Description:
+;
+;===========================================
+*/
+
+
+const header = require('../baker-header.js');
+
+console.log(header.display("Tiffany", "Baker", "assignment"));
+
+
+//variables
+
+
+//output
---
+++ week-6/exercise6.2.js
@@ -0,0 +1,40 @@
+/*
+============================================
+; Title: exercise 6.2
+; Author: Tiffany Baker
+; Date:7/4/2020
+; Modified By:
+; Description: this program demonstrates exception handling.
+;
+;===========================================
+*/
+
+
+const header = require('../baker-header.js');
+
+console.log(header.display("Tiffany", "Baker", "exercise 6.2"));
+
+
+
+
+try{
+  //variables
+  var a = "10";
+  var b = "12"
+  var err = console.log("catch clause: Sorry you cant do that...");
+
+
+if(a !== b) throw "error";
+
+}
+catch(err){
+
+
+}
+finally{
+  console.log("Finally clause reached");
+
+}
+
+
+//output
